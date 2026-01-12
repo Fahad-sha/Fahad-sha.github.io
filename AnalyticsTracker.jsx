d16dd1512effbccaabf59050cdc2587303d3d11f
Fahad Shaikh <36107958+Fahad-sha@users.noreply.github.com>
2026-01-12 16:21:28 -0500

    Update AnalyticsTracker.jsx

--- AnalyticsTracker.jsx
+++ AnalyticsTracker.jsx
@@ -5,8 +5,10 @@ export default function AnalyticsTracker() {
   const location = useLocation();
 
   useEffect(() => {
-    if (typeof window.gtag === "function") {
-      window.gtag("config", "G-XXXXXXX", { page_path: location.pathname });
+    if (window.gtag) {
+      window.gtag("config", "G-QGEFZEX991", {
+        page_path: location.pathname,
+      });
     }
   }, [location]);
 
