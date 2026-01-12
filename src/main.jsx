96a4c8de3bedb92a2349581e4466b4860cca38a9
Fahad Shaikh <36107958+Fahad-sha@users.noreply.github.com>
2026-01-12 16:33:33 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
+import AnalyticsTracker from "./AnalyticsTracker.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
+      <AnalyticsTracker />
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
